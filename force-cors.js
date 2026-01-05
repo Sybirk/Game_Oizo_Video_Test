6a94d64bcc3775ca280c9296ed6e52086d6e895b
Sybirk <115795460+Sybirk@users.noreply.github.com>
2026-01-05 01:37:14 +0100

    Update force-cors.js

--- force-cors.js
+++ force-cors.js
@@ -1,15 +1,25 @@
 export default async (request, context) => {
-  const response = await context.next();
+  // Récupère la réponse standard prévue
+  const response = await context.next()
+  // Duplique les headers et ajoute CORS
+  const headers = new Headers(response.headers)
+
+  headers.set("Access-Control-Allow-Origin", "*")
+  headers.set("Access-Control-Allow-Methods", "GET, OPTIONS")
+  headers.set("Access-Control-Allow-Headers", "*")
+
+  if (request.method === "OPTIONS") {
+    // Répond aux requêtes préliminaires (OPTIONS)
+    return new Response(null, { status: 204, headers })
+  }
+
+  // Retourne la réponse avec les nouveaux headers
   return new Response(response.body, {
-    headers: {
-      ...Object.fromEntries(response.headers),
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "*"
-    }
-  });
-};
+    status: response.status,
+    headers
+  })
+}
 
 export const config = {
   path: "/*"
-};
+}
